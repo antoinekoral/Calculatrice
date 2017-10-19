ca3ed50b9250381f6d4ee4d752b60ea442cddd5e
ToinouKoko <antoine.koral@gmail.com>
2017-10-19 12:06:14 +0200

    Mise en place des différents éléments

---
+++ app/src/main/java/ecl/calculatrice/MainActivity.java
@@ -0,0 +1,19 @@
+package ecl.calculatrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calcul() {
+
+    }
+
+
+}
